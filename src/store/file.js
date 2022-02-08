7438981e78473a907515975947ac0d22279dfc27
lizheming <i@imnerd.org>
2022-02-08 23:24:19 +0800

    chore: add csv support

--- src/store/file.js
+++ src/store/file.js
@@ -1,21 +1,56 @@
 const path = require('path');
 const { constants } = require('fs');
 const fs = require('fs/promises');
-
+const { parseString, writeToString } = require('fast-csv');
 module.exports = class FileStore {
   constructor({type, dir}) {
-    this.filename = path.join(dir, `${type}.json`);
+    this.filename = path.join(dir, `${type}.csv`);
   }
 
   async parse(data) {
-    return JSON.parse(data);
+    return new Promise((resolve, reject) => {
+      const ret = [];
+      parseString(data, {
+        headers: true,
+      })
+        .on('error', reject)
+        .on('data', (row) => ret.push(row))
+        .on('end', () => resolve(ret));
+    });
   }
 
   async stringify(data) {
-    return JSON.stringify(data, null, '\t');
+    return writeToString(data, {
+      headers: true,
+      writeHeaders: true,
+    });
   }
 
-  format(item) {
+  format({
+    id,
+    create_time,
+    subject,
+  }) {
+    if(subject.title === '暴走财神3')
+      console.log(JSON.stringify(subject, null, '\t'));
+    const item = {
+      id,
+      title: subject.title,
+      subtitle: subject.card_subtitle,
+      intro: subject.intro,
+      poster: subject.cover_url,
+      pubdate: subject.pubdate[0],
+      url: subject.url,
+      rating: subject.rating.value,
+      genres: JSON.stringify(subject.genres),
+      star: subject.rating.star_count,
+      star_time: create_time,
+    };
+
+    for(const i in item) {
+      if(item[i] === undefined) delete item[i];
+    }
+
     return item;
   }
 
