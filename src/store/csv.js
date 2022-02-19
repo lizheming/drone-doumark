e5614ecc87da0b128965903c9122c55988ff53b1
lizheming <i@imnerd.org>
2022-02-19 13:27:36 +0800

    fix: fix star_time compare exception

--- src/store/csv.js
+++ src/store/csv.js
@@ -1,8 +1,11 @@
 const path = require('path');
+const dayjs = require('dayjs');
 const { constants } = require('fs');
 const fs = require('fs/promises');
 const fse = require('fs-extra');
 const { parseString, writeToString } = require('fast-csv');
+const customParseFormat = require('dayjs/plugin/customParseFormat');
+dayjs.extend(customParseFormat);
 module.exports = class FileStore {
   constructor({type, dir}) {
     this.filename = path.join(dir, `${type}.csv`);
@@ -32,8 +35,6 @@ module.exports = class FileStore {
     create_time,
     subject,
   }) {
-    if(subject.title === '暴走财神3')
-      console.log(JSON.stringify(subject, null, '\t'));
     const item = {
       id,
       title: subject.title,
@@ -59,7 +60,10 @@ module.exports = class FileStore {
     const isExist = await fs.access(this.filename, constants.F_OK).then(() => true, () => false);
     const text = isExist ? (await fs.readFile(this.filename, 'utf-8')) : '[]';
     this._data = await this.parse(text);
-    return this._data;
+    return this._data.map(item => {
+      item.star_time = dayjs(item.star_time, 'YYYY-MM-DD HH:mm:ss').valueOf();
+      return item;
+    });
   }
 
   async set(data) {
--- src/store/json.js
+++ src/store/json.js
@@ -1,7 +1,10 @@
 const path = require('path');
+const dayjs = require('dayjs');
 const { constants } = require('fs');
 const fs = require('fs/promises');
 const fse = require('fs-extra');
+const customParseFormat = require('dayjs/plugin/customParseFormat');
+dayjs.extend(customParseFormat);
 
 module.exports = class FileStore {
   constructor({type, dir}) {
@@ -24,7 +27,10 @@ module.exports = class FileStore {
     const isExist = await fs.access(this.filename, constants.F_OK).then(() => true, () => false);
     const text = isExist ? (await fs.readFile(this.filename, 'utf-8')) : '[]';
     this._data = await this.parse(text);
-    return this._data;
+    return this._data.map(item => {
+      item.star_time = dayjs(item.star_time, 'YYYY-MM-DD HH:mm:ss').valueOf();
+      return item;
+    });
   }
 
   async set(data) {
